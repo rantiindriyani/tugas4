d12c7a292d8388298aff1d9af7bbd59395f46a0f
rantiindriyani <121720015+rantiindriyani@users.noreply.github.com>
2023-10-18 06:16:00 +0700

    Add files via upload
    tugas sesi 4 if else ,switch case ranti indriyani ti22c

---
+++ tugass4.js
@@ -0,0 +1,161 @@
+/* Pendapatan harian salesman
+N0 1. IF ELSE */
+const pendapatanHarian = 200000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uangJasa = 0;
+let komisi = 0;
+
+// Cek apakah pendapatan harian kurang dari atau sama dengan Rp. 200.000
+if (pendapatanHarian <= 200000) {
+  // Jika pendapatan kurang dari atau sama dengan Rp. 200.000, berikan uang jasa Rp. 10.000
+  uangJasa = 10000;
+  // Hitung komisi sebesar 10% dari pendapatan
+  komisi = 0.1 * pendapatanHarian;
+} else {
+    // Jika pendapatan lebih dari Rp. 200.000, hanya hitung komisi sebesar 10%
+    komisi = 0.1 * pendapatanHarian;
+  }
+  
+  // Hitung total bayaran
+  const totalBayaran = uangJasa + komisi;
+  
+  // Tampilkan hasil
+  console.log("Pendapatan Harian: Rp. " + pendapatanHarian);
+  console.log("Uang Jasa: Rp. " + uangJasa);
+  console.log("Komisi: Rp. " + komisi);
+  console.log("Total Bayaran: Rp. " + totalBayaran);
+
+// Pendapatan harian salesman
+const pendapatanHariann = 200000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uangJasaa = 0;
+let komisii = 0;
+
+/* NO 1 
+Menggunakan switch case untuk menentukan jumlah uang jasa dan komisi */
+switch (true) {
+  case pendapatanHariann <= 200000:
+    uangJasaa = 10000;
+    komisii = 0.1 * pendapatanHariann;
+    break;
+    default:
+    komisi = 0.1 * pendapatanHariann;
+}
+
+// Hitung total bayaran
+const totalBayarann = uangJasaa + komisii;
+
+// Tampilkan hasil
+console.log("Pendapatan Harian: Rp. " + pendapatanHariann);
+console.log("Uang Jasa: Rp. " + uangJasaa);
+console.log("Komisi: Rp. " + komisii);
+console.log("Total Bayaran: Rp. " + totalBayarann);
+
+// NO 2 MENGGUNAKAN IF ELSE
+
+// Pendapatan harian salesman
+const pendapatanHari = 250000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uangJasa1 = 0;
+let komisi1 = 0;
+
+// Cek apakah pendapatan harian lebih dari Rp. 200.000
+if (pendapatanHari > 200000) {
+  // Jika pendapatan lebih dari Rp. 200.000, berikan uang jasa Rp. 20.000
+  uangJasa1 = 20000;
+  // Hitung komisi sebesar 15% dari pendapatan
+  komisi1 = 0.15 * pendapatanHari;
+} else {
+    // Jika pendapatan kurang dari atau sama dengan Rp. 200.000, hanya hitung komisi sebesar 15%
+    komisi1 = 0.15 * pendapatanHari;
+  }
+  
+  // Hitung total bayaran
+  const totalBayaran1 = uangJasa1 + komisi1;
+  
+  // Tampilkan hasil
+  console.log("Pendapatan Harian: Rp. " + pendapatanHari);
+  console.log("Uang Jasa: Rp. " + uangJasa1);
+  console.log("Komisi: Rp. " + komisi1);
+  console.log("Total Bayaran: Rp. " + totalBayaran1);
+
+//NO 2 MENGGUNAKAN SWICH CASE
+
+// Pendapatan harian salesman
+const pendapatanHarian1 = 250000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uangJasaaa = 0;
+let komisiii = 0;
+
+// Menggunakan switch case untuk menentukan jumlah uang jasa dan komisi
+switch (true) {
+  case pendapatanHarian1 > 200000:
+    uangJasaaa = 20000;
+    komisiii = 0.15 * pendapatanHarian1;
+    break;
+  default:
+    komisiii = 0.15 * pendapatanHarian1;
+}
+
+// Hitung total bayaran
+const totalBayarannn = uangJasaaa + komisiii;
+
+// Tampilkan hasil
+console.log("Pendapatan Harian: Rp. " + pendapatanHarian1);
+console.log("Uang Jasa: Rp. " + uangJasaaa);
+console.log("Komisi: Rp. " + komisiii);
+console.log("Total Bayaran: Rp. " + totalBayarannn);
+
+// NO 3 menggunakan if else 
+// Pendapatan harian salesman
+const pendapataanHarian = 600000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uangJaasa = 0;
+let komissi = 0;
+
+// Cek apakah pendapatan harian lebih dari Rp. 500.000
+if (pendapataanHarian > 500000) {
+  // Jika pendapatan lebih dari Rp. 500.000, berikan uang jasa Rp. 30.000
+  uangJaasa = 30000;
+  // Hitung komisi sebesar 20% dari pendapatan
+  komissi = 0.2 * pendapataanHarian;
+} else {
+    // Jika pendapatan kurang dari atau sama dengan Rp. 500.000, hanya hitung komisi sebesar 20%
+    komisi = 0.2 * pendapatanHarian;
+}
+  
+// Hitung total bayaran
+const totaalBayaran = uangJaasa + komissi;
+  
+// Tampilkan hasil
+console.log("Pendapatan Harian: Rp. " + pendapataanHarian);
+console.log("Uang Jasa: Rp. " + uangJaasa);
+console.log("Komisi: Rp. " + komissi);
+console.log("Total Bayaran: Rp. " + totaalBayaran);
+
+// N0 3 menggunakan switch case
+// Pendapatan harian salesman
+const pendapattanHarian = 600000; // Ganti dengan jumlah pendapatan yang sesuai
+
+// Inisialisasi variabel uangJasa dan komisi
+let uanngJasa = 0;
+let kommisi = 0;
+
+// Menggunakan switch case untuk menentukan jumlah uang jasa dan komisi
+switch (true) {
+  case pendapattanHarian > 500000:
+    uanngJasa = 30000;
+    kommisi = 0.2 * pendapattanHarian;
+    break;
+  default:
+    kommisi = 0.2 * pendapattanHarian;
+}
+
+
+
+
